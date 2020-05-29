73c72acc51682fa483ff5309681b85190ee2434b
Karthikeyan Nithiyanandam <karthikeyan.ng@gmail.com>
2020-05-29 20:51:44 +0530

    Created Project using start.spring.io: Initial Commit

---
+++ src/main/java/com/techstack/batch/LearnAndApplySpringBatchApplication.java
@@ -0,0 +1,13 @@
+package com.techstack.batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnAndApplySpringBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnAndApplySpringBatchApplication.class, args);
+	}
+
+}
