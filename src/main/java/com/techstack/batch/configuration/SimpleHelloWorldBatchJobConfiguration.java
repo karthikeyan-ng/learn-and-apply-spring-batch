c09e60e56bc3ec3827dcfd9236a3db93ae0f5fc7
Karthikeyan Nithiyanandam <karthikeyan.ng@gmail.com>
2020-06-01 16:23:02 +0530

    Renamed the class

--- src/main/java/com/techstack/batch/configuration/SimpleHelloWorldBatchJobConfiguration.java
+++ src/main/java/com/techstack/batch/configuration/SimpleHelloWorldBatchJobConfiguration.java
@@ -1,27 +1,24 @@
 package com.techstack.batch.configuration;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.StepContribution;
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
-@EnableBatchProcessing
-public class BatchJobConfiguration {
+@RequiredArgsConstructor
+//@Configuration
+public class SimpleHelloWorldBatchJobConfiguration {
 
-    @Autowired
-    private JobBuilderFactory jobBuilderFactory;
+    private final JobBuilderFactory jobBuilderFactory;
 
-    @Autowired
-    private StepBuilderFactory stepBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
 
     @Bean
     public Step step1() {
