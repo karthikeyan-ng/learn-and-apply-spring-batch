1eb9cab166673b18aa894c774f5f4e6c43ae8521
Karthikeyan Nithiyanandam <karthikeyan.ng@gmail.com>
2020-06-03 22:01:57 +0530

    Commented @Configuration

--- src/main/java/com/techstack/batch/configuration/nested/ChildJobConfiguration.java
+++ src/main/java/com/techstack/batch/configuration/nested/ChildJobConfiguration.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @RequiredArgsConstructor
-@Configuration
+//@Configuration
 public class ChildJobConfiguration {
 
     private final JobBuilderFactory jobBuilderFactory;
--- src/main/java/com/techstack/batch/configuration/nested/ParentJobConfiguration.java
+++ src/main/java/com/techstack/batch/configuration/nested/ParentJobConfiguration.java
@@ -15,7 +15,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.PlatformTransactionManager;
 
 @RequiredArgsConstructor
-@Configuration
+//@Configuration
 public class ParentJobConfiguration {
 
     private final JobBuilderFactory jobBuilderFactory;
